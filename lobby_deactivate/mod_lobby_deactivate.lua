c933763d3b142f53eb32a196753def81aa18765e
emrah <emrah.eryilmaz@nordeck.net>
2024-04-25 16:28:04 +0300

    feat(lobby_deactivate): initialize

---
+++ lobby_deactivate/mod_lobby_deactivate.lua
@@ -0,0 +1,42 @@
+--- Plugin to deactivate the lobby after the first join.
+---
+--- This module should be added to the main muc component.
+---
+
+local LOGLEVEL = "debug";
+
+local is_healthcheck_room = module:require "util".is_healthcheck_room;
+
+module:log("info", "loaded");
+
+module:hook("muc-occupant-join", function (event)
+    local room, origin = event.room, event.origin;
+
+    if is_healthcheck_room(room.jid) then
+        return;
+    end
+
+    -- do nothing if already checked
+    if room._data.lobby_deactivated then
+        return
+    end
+    room._data.lobby_deactivated = true
+
+    -- do nothing if the lobby is not activate
+    if room._data.lobbyroom == nil then
+        module:log(LOGLEVEL, "no active lobby - %s", room.jid)
+        return
+    end
+
+    -- deactivate the lobby if it is not enabled explicitly
+    local context_room = origin.jitsi_meet_context_room;
+    if not context_room or context_room["lobby"] ~= true then
+        room:set_members_only(false)
+        prosody.events.fire_event('destroy-lobby-room', {
+            room = room,
+            newjid = room.jid,
+       });
+    end
+end, -2);
+--- Run just before lobby_bypass (priority -3), lobby(-4) and members_only (-5).
+--- Must run after token_verification (99), max_occupants (10), allowners (2).
