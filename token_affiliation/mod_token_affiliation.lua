9a5127cd26cb956c75b4f11c330cda7927c3f1b9
emrah <emrah.eryilmaz@conceptboard.com>
2025-05-21 17:10:31 +0300

    refactor(token_affiliation): is_admin from util

--- token_affiliation/mod_token_affiliation.lua
+++ token_affiliation/mod_token_affiliation.lua
@@ -7,19 +7,16 @@ local DISABLE_CASCADING_SET = module:get_option_boolean(
     "disable_cascading_set", false
 )
 
-local is_admin = require "core.usermanager".is_admin
+local util = module:require 'util';
+local is_admin = util.is_admin;
 local is_healthcheck_room = module:require "util".is_healthcheck_room
 local timer = require "util.timer"
 module:log(LOGLEVEL, "loaded")
 
-local function _is_admin(jid)
-    return is_admin(jid, module.host)
-end
-
 module:hook("muc-occupant-joined", function (event)
     local room, occupant = event.room, event.occupant
 
-    if is_healthcheck_room(room.jid) or _is_admin(occupant.jid) then
+    if is_healthcheck_room(room.jid) or is_admin(occupant.bare_jid) then
         module:log(LOGLEVEL, "skip affiliation, %s", occupant.jid)
         return
     end
