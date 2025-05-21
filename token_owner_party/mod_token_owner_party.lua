afd724f6ef935ec7e10ca5cdaae21829e70a8631
emrah <emrah.eryilmaz@conceptboard.com>
2025-05-21 17:41:26 +0300

    refactor(token_owner_party): is_admin from util

--- token_owner_party/mod_token_owner_party.lua
+++ token_owner_party/mod_token_owner_party.lua
@@ -1,22 +1,19 @@
 local LOGLEVEL = "debug"
 local TIMEOUT = module:get_option_number("party_check_timeout", 60)
 
-local is_admin = require "core.usermanager".is_admin
-local is_healthcheck_room = module:require "util".is_healthcheck_room
+local util = module:require 'util';
+local is_admin = util.is_admin;
+local is_healthcheck_room = util.is_healthcheck_room
 local it = require "util.iterators"
 local st = require "util.stanza"
 local timer = require "util.timer"
 module:log(LOGLEVEL, "loaded")
 
-local function _is_admin(jid)
-    return is_admin(jid, module.host)
-end
-
 module:hook("muc-occupant-pre-join", function (event)
     local room, stanza = event.room, event.stanza
     local user_jid = stanza.attr.from
 
-    if is_healthcheck_room(room.jid) or _is_admin(user_jid) then
+    if is_healthcheck_room(room.jid) or is_admin(user_jid) then
         module:log(LOGLEVEL, "location check, %s", user_jid)
         return
     end
@@ -46,7 +43,7 @@ end)
 module:hook("muc-occupant-left", function (event)
     local room, occupant = event.room, event.occupant
 
-    if is_healthcheck_room(room.jid) or _is_admin(occupant.jid) then
+    if is_healthcheck_room(room.jid) or is_admin(occupant.bare_jid) then
         return
     end
 
@@ -60,7 +57,7 @@ module:hook("muc-occupant-left", function (event)
 
     -- check if there is any other owner here
     for _, o in room:each_occupant() do
-        if not _is_admin(o.jid) then
+        if not is_admin(o.bare_jid) then
             if room:get_affiliation(o.jid) == "owner" then
                 module:log(LOGLEVEL, "an owner is still here, %s", o.jid)
                 return
@@ -82,7 +79,7 @@ module:hook("muc-occupant-left", function (event)
         -- last check before destroying the room
         -- if an owner is still here, cancel
         for _, o in room:each_occupant() do
-            if not _is_admin(o.jid) then
+            if not is_admin(o.bare_jid) then
                 if room:get_affiliation(o.jid) == "owner" then
                     module:log(
                         LOGLEVEL,
