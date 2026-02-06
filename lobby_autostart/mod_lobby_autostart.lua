57d6e7b7f5e7b228278ec3a79952a429eab91e0b
emrah <emrah.eryilmaz@conceptboard.com>
2026-02-06 17:16:19 +0300

    refactor(lobby_autostart): format issues

--- lobby_autostart/mod_lobby_autostart.lua
+++ lobby_autostart/mod_lobby_autostart.lua
@@ -1,7 +1,8 @@
 -- This module auto-activates lobby for all rooms.
 --
--- IMPORTANT: do not use this unless you have some mechanism for moderators to bypass the lobby, otherwise everybody
---            stops at the lobby with nobody to admit them.
+-- IMPORTANT:
+--   Do not use this unless you have some mechanism for moderators to bypass the
+--   lobby. Otherwise everybody stops at the lobby with nobody to admit them.
 --
 -- This module should be added to the main muc component.
 --
@@ -10,7 +11,6 @@ local util = module:require "util";
 local is_healthcheck_room = util.is_healthcheck_room;
 
 module:log("info", "loaded")
-
 
 module:hook("muc-room-pre-create", function (event)
    local room = event.room;
@@ -21,4 +21,3 @@ module:hook("muc-room-pre-create", function (event)
 
     prosody.events.fire_event("create-persistent-lobby-room", { room = room; });
 end);
-
